9bd1d85145e408b09df561ced6ec19745cdf17d9
Rafael <rafaelramalhosilveira@gmail.com>
2024-08-09 06:00:56 -0400

    feat: adiciona TaskController para gerenciar tarefas

---
+++ src/main/java/br/com/devsilveira/task/controller/TaskController.java
@@ -0,0 +1,32 @@
+package br.com.devsilveira.task.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+/*
+*  A Classe Controller das tarefas
+*
+* */
+@RestController
+@RequestMapping("/task")
+public class TaskController {
+
+    @PostMapping
+    public void register(){
+
+    }
+
+    @GetMapping
+    public void list(){
+
+    }
+
+    @PutMapping
+    public void update(){
+
+    }
+
+    @DeleteMapping
+    public void delete(){
+
+    }
+}
